829c0ef710b7654640a2e9073cd2d90ce6011e98
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-08 23:58:26 +0900

    feat: add PageWrapper component for page transition effects; refactor Hero and Main components to use PageWrapper

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import ThemeToggle from "../components/ThemeToggle";
 import gsap from "gsap";
 import Button from "../components/Button";
+import PageWrapper from "../transition/PageWrapper";
 
 const Hero = () => {
   const introRef = useRef<HTMLDivElement>(null);
@@ -28,43 +29,45 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen flex flex-col">
-      <div className="relative w-full">
-        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 text-black dark:text-white">
-          <Navbar />
-        </div>
-        <div className="absolute top-4 right-6">
-          <ThemeToggle />
-        </div>
-      </div>
-
-      <div className="flex flex-1 items-center px-10">
-        <div className="text-left">
-          <div
-            className="text-6xl md:text-9xl font-extrabold text-gray-900 dark:text-white"
-            ref={introRef}
-          >
-            Welcome!
+    <PageWrapper>
+      <div className="w-full h-screen flex flex-col">
+        <div className="relative w-full">
+          <div className="absolute top-4 left-1/2 transform -translate-x-1/2 text-black dark:text-white">
+            <Navbar />
           </div>
-          <div
-            className="text-3xl md:text-7xl font-bold text-gray-700 dark:text-gray-300 mt-4"
-            ref={textRef}
-          >
-            This is a domain checker website
+          <div className="absolute top-4 right-6">
+            <ThemeToggle />
           </div>
-          <p
-            className="text-base md:text-3xl mt-6 max-w-4xl text-gray-600 dark:text-gray-400"
-            ref={descRef}
-          >
-            You can use this website to check if a domain is available or not.
-          </p>
+        </div>
+
+        <div className="flex flex-1 items-center px-10">
+          <div className="text-left">
+            <div
+              className="text-6xl md:text-9xl font-extrabold text-gray-900 dark:text-white"
+              ref={introRef}
+            >
+              Welcome!
+            </div>
+            <div
+              className="text-3xl md:text-7xl font-bold text-gray-700 dark:text-gray-300 mt-4"
+              ref={textRef}
+            >
+              This is a domain checker website
+            </div>
+            <p
+              className="text-base md:text-3xl mt-6 max-w-4xl text-gray-600 dark:text-gray-400"
+              ref={descRef}
+            >
+              You can use this website to check if a domain is available or not.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <Button />
         </div>
       </div>
-
-      <div className="flex items-center justify-center">
-        <Button />
-      </div>
-    </div>
+    </PageWrapper>
   );
 };
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import { useTheme } from "../hooks/useTheme";
 import { House } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import PageWrapper from "../transition/PageWrapper";
 
 const Main = () => {
   const [domain, setDomain] = useState("");
@@ -26,55 +27,57 @@ const Main = () => {
   };
 
   return (
-    <div className="flex flex-col gap-10 mx-10 mt-10">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <House
-            size={40}
-            className="cursor-pointer"
-            onClick={() => navigate("/")}
-          />
+    <PageWrapper>
+      <div className="flex flex-col gap-10 mx-10 mt-10">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <House
+              size={40}
+              className="cursor-pointer"
+              onClick={() => navigate("/")}
+            />
 
-          <div className="w-0.5 h-12 bg-black dark:bg-white"></div>
+            <div className="w-0.5 h-12 bg-black dark:bg-white"></div>
 
-          <Navbar />
+            <Navbar />
+          </div>
+          <ThemeToggle />
         </div>
-        <ThemeToggle />
-      </div>
 
-      <div className="flex items-center justify-center text-6xl font-bold mt-20">
-        Enter the domain you want to check
-      </div>
+        <div className="flex items-center justify-center text-6xl font-bold mt-20">
+          Enter the domain you want to check
+        </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <div className="flex w-[90%] md:w-1/3 gap-2">
-          <input
-            value={domain}
-            onChange={(e) => setDomain(e.target.value)}
-            placeholder="example.com"
-            type="text"
-            className="flex-1 px-4 py-2 
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <div className="flex w-[90%] md:w-1/3 gap-2">
+            <input
+              value={domain}
+              onChange={(e) => setDomain(e.target.value)}
+              placeholder="example.com"
+              type="text"
+              className="flex-1 px-4 py-2 
         text-black dark:text-white 
         bg-white dark:bg-gray-950 
         rounded-md border border-gray-300 dark:border-gray-700 
         focus:outline-none focus:ring-2 focus:ring-gray-500"
-          />
+            />
+            <button
+              onClick={() => setDomain("")}
+              className="px-4 py-2 bg-red-500 text-white rounded-md font-semibold hover:bg-red-600"
+            >
+              Clear
+            </button>
+          </div>
+
           <button
-            onClick={() => setDomain("")}
-            className="px-4 py-2 bg-red-500 text-white rounded-md font-semibold hover:bg-red-600"
+            className="w-[90%] md:w-1/3 px-4 py-2 bg-black dark:bg-white text-white dark:text-black text-lg rounded-md font-bold"
+            onClick={handleCheckDomain}
           >
-            Clear
+            Check
           </button>
         </div>
-
-        <button
-          className="w-[90%] md:w-1/3 px-4 py-2 bg-black dark:bg-white text-white dark:text-black text-lg rounded-md font-bold"
-          onClick={handleCheckDomain}
-        >
-          Check
-        </button>
       </div>
-    </div>
+    </PageWrapper>
   );
 };
 
