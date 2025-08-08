7afe405e50e575c02b452cec8f8f5109daba19e0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-08-08 18:05:08 +0900

    Add OFL.txt for Forum font under SIL Open Font License, Version 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white flex items-center justify-center">
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white flex items-center justify-center">
       <ThemeToggle />
     </div>
   );
